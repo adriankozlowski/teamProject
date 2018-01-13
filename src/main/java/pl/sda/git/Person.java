c1a0c64e0d1dd0d88cf102b4455b2b31874a587c
wozog <hugowojtek@o2.pl>
2018-01-13 15:32:10 +0100

    modelDanych

---
+++ src/main/java/pl/sda/git/Person.java
@@ -0,0 +1,90 @@
+package pl.sda.git;
+
+import java.util.Random;
+
+public class Person {
+    private int losIdent;
+    private String imie;
+    private String nazwisko;
+    private int rokUrodz;
+    private String miastoZamiesz;
+    private String ulica;
+    private int kodPocztowy;
+    private String stanowisko;
+
+    public Person(String imie, String nazwisko, int rokUrodz, String miastoZamiesz, String ulica, int kodPocztowy, String stanowisko) {
+        Random random = new Random();
+        this.losIdent = random.nextInt(100);
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.rokUrodz = rokUrodz;
+        this.miastoZamiesz = miastoZamiesz;
+        this.ulica = ulica;
+        this.kodPocztowy = kodPocztowy;
+        this.stanowisko = stanowisko;
+    }
+
+    public int getLosIdent() {
+        return losIdent;
+    }
+
+    public void setLosIdent(int losIdent) {
+        this.losIdent = losIdent;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getRokUrodz() {
+        return rokUrodz;
+    }
+
+    public void setRokUrodz(int rokUrodz) {
+        this.rokUrodz = rokUrodz;
+    }
+
+    public String getMiastoZamiesz() {
+        return miastoZamiesz;
+    }
+
+    public void setMiastoZamiesz(String miastoZamiesz) {
+        this.miastoZamiesz = miastoZamiesz;
+    }
+
+    public String getUlica() {
+        return ulica;
+    }
+
+    public void setUlica(String ulica) {
+        this.ulica = ulica;
+    }
+
+    public int getKodPocztowy() {
+        return kodPocztowy;
+    }
+
+    public void setKodPocztowy(int kodPocztowy) {
+        this.kodPocztowy = kodPocztowy;
+    }
+
+    public String getStanowisko() {
+        return stanowisko;
+    }
+
+    public void setStanowisko(String stanowisko) {
+        this.stanowisko = stanowisko;
+    }
+}
