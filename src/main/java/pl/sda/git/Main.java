316303615870b06a1a8be765f4406bb676311193
wozog <hugowojtek@o2.pl>
2018-01-14 10:38:59 +0100

    modelDanych

--- src/main/java/pl/sda/git/Main.java
+++ src/main/java/pl/sda/git/Main.java
@@ -3,34 +3,62 @@ package pl.sda.git;
 import java.util.Scanner;
 
 public class Main {
+    static PersonList personList = new PersonList();
+
     public static void main(String[] args) {
         showmenu();
     }
-    static PersonList personList = new PersonList();
+
     private static void showmenu() {
         Scanner scanner = new Scanner(System.in);
-        int number = 0;
         int menunumber = 0;
+        int number = 0;
+        boolean exit = false;
 
-        System.out.println(++menunumber + ". Dodaj osobę");
-        System.out.println(++menunumber + ". Pokaż listę osób");
-        System.out.println(++menunumber + ". Eksport bazy do pliku");
-        System.out.println(++menunumber + ". Import bazy z pliku");
-        System.out.println(++menunumber + ". Sortowanie po kluczu");
-        System.out.println(++menunumber + ". Zamknij program");
-        try {
-            while (number > menunumber || number < 1){
-                System.out.println("Wpisz twój wybór: ");
-                number = scanner.nextInt();}
-        } catch (Exception e) {
-            Main.showmenu();
-        }
+        while (!exit) {
+            menunumber = 0;
+            number = 0;
 
-        switch (number) {
-            case 1: {
-                //Wywołanie dodania osoby
+            System.out.println(++menunumber + ". Dodaj osobę");
+            System.out.println(++menunumber + ". Pokaż listę osób");
+            System.out.println(++menunumber + ". Eksport bazy do pliku");
+            System.out.println(++menunumber + ". Import bazy z pliku");
+            System.out.println(++menunumber + ". Sortowanie po kluczu");
+            System.out.println(++menunumber + ". Zamknij program");
+            try {
+                while (number > menunumber || number < 1) {
+                    System.out.println("Wpisz twój wybór: ");
+                    number = scanner.nextInt();
+                }
+            } catch (Exception e) {
+                scanner.nextLine();
+            }
 
-                personList.addPerson();
+            switch (number) {
+                case 1: {
+                    //Wywołanie dodania osoby
+                    personList.addPerson();
+                    break;
+                }
+                case 2: {
+                    personList.showList();
+
+<<<<<<< .merge_file_a03824
+                    //Wywołanie  pokazania listy osób
+                    break;
+                }
+                case 3: {
+                    ////Wywołanie  eksportu do pliku
+                    break;
+                }
+                case 4: {
+                    //Wywołanie importu z bazy
+                    break;
+                }
+=======
+                System.out.println("------------");
+
+                Main.showmenu();
                 break;
             }
             case 2: {
@@ -46,25 +74,29 @@ public class Main {
                 //Wywołanie importu z bazy
                 break;
             }
+>>>>>>> .merge_file_a03152
 
 
-            case 5: {
-                //Wywołanie sortowania
+                case 5: {
+                    //Wywołanie sortowania
 
-                break;
+                    break;
+                }
+
+
+                case 6: {
+                    //Koniec programu
+                    exit = true;
+                    break;
+                }
+
+                default: {
+                    break;
+                }
             }
-
-
-            case 6: {
-                //Koniec programu
-                System.out.println("Program się zamyka!");
-                break;
-            }
-
-        default:{
-        Main.showmenu();
-        break;}
+        }
+        System.out.println("Koniec programu!");
     }
 
 }
-}
+
