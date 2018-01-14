45ca6b6c796a0194aca698bdd29c8065eb8720c2
Michal_Ostrega <michalostrega@gmail.com>
2018-01-14 10:00:16 +0100

    Dodanie pętli while do Menu

--- src/main/java/pl/sda/git/Main.java
+++ src/main/java/pl/sda/git/Main.java
@@ -3,68 +3,77 @@ package pl.sda.git;
 import java.util.Scanner;
 
 public class Main {
+    static PersonList personList = new PersonList();
+
     public static void main(String[] args) {
         showmenu();
     }
-    static PersonList personList = new PersonList();
+
     private static void showmenu() {
         Scanner scanner = new Scanner(System.in);
-        int number = 0;
         int menunumber = 0;
+        int number = 0;
+        boolean exit = false;
 
-        System.out.println(++menunumber + ". Dodaj osobę");
-        System.out.println(++menunumber + ". Pokaż listę osób");
-        System.out.println(++menunumber + ". Eksport bazy do pliku");
-        System.out.println(++menunumber + ". Import bazy z pliku");
-        System.out.println(++menunumber + ". Sortowanie po kluczu");
-        System.out.println(++menunumber + ". Zamknij program");
-        try {
-            while (number > menunumber || number < 1){
-                System.out.println("Wpisz twój wybór: ");
-                number = scanner.nextInt();}
-        } catch (Exception e) {
-            Main.showmenu();
+        while (!exit) {
+            menunumber = 0;
+            number = 0;
+
+            System.out.println(++menunumber + ". Dodaj osobę");
+            System.out.println(++menunumber + ". Pokaż listę osób");
+            System.out.println(++menunumber + ". Eksport bazy do pliku");
+            System.out.println(++menunumber + ". Import bazy z pliku");
+            System.out.println(++menunumber + ". Sortowanie po kluczu");
+            System.out.println(++menunumber + ". Zamknij program");
+            try {
+                while (number > menunumber || number < 1) {
+                    System.out.println("Wpisz twój wybór: ");
+                    number = scanner.nextInt();
+                }
+            } catch (Exception e) {
+                scanner.nextLine();
+            }
+
+            switch (number) {
+                case 1: {
+                    //Wywołanie dodania osoby
+                    personList.addPerson();
+                    break;
+                }
+                case 2: {
+                    //Wywołanie  pokazania listy osób
+                    break;
+                }
+                case 3: {
+                    ////Wywołanie  eksportu do pliku
+                    break;
+                }
+                case 4: {
+                    //Wywołanie importu z bazy
+                    break;
+                }
+
+
+                case 5: {
+                    //Wywołanie sortowania
+
+                    break;
+                }
+
+
+                case 6: {
+                    //Koniec programu
+                    exit = true;
+                    break;
+                }
+
+                default: {
+                    break;
+                }
+            }
         }
-
-        switch (number) {
-            case 1: {
-                //Wywołanie dodania osoby
-
-                personList.addPerson();
-                break;
-            }
-            case 2: {
-                //Wywołanie  pokazania listy osób
-                personList.showList();
-                break;
-            }
-            case 3: {
-                ////Wywołanie  eksportu do pliku
-                break;
-            }
-            case 4: {
-                //Wywołanie importu z bazy
-                break;
-            }
-
-
-            case 5: {
-                //Wywołanie sortowania
-
-                break;
-            }
-
-
-            case 6: {
-                //Koniec programu
-                System.out.println("Program się zamyka!");
-                break;
-            }
-
-        default:{
-        Main.showmenu();
-        break;}
+        System.out.println("Koniec programu!");
     }
 
 }
-}
+
