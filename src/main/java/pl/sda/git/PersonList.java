3c7b08bf8e15a2b20db336f2c3c3a04f08b956d0
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-01-14 09:49:36 +0100

    Person list

--- src/main/java/pl/sda/git/PersonList.java
+++ src/main/java/pl/sda/git/PersonList.java
@@ -6,18 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PersonList {
+    private List<Person> list = new ArrayList<Person>();
 
-    public static void showList() {
+    public void showList() {
 
-
-    List<Person> list = new ArrayList<Person>();
-    int i = 0;
-//    for(i = 0; i<list.length; i++){
-        System.out.println(list);
+        for (Person person : list) {
+            System.out.println(person.getImie());
+            //itd
+        }
     }
-
 
     public void addPerson() {
         System.out.println("Podaj imie");
+        //dodaje perosny do listy
     }
 }
